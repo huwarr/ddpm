988f0000145d87fc0607c158b88daf395cc7182c
huwarr <mikhalmatova@gmail.com>
2022-11-11 13:53:32 +0300

    fix bugs in run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import math
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs")
+parser.add_argument("--n_epochs", type=int, default=5, help="number of epochs")
 parser.add_argument("--use_wandb", type=bool, default=False, help="whether to use wandb")
 parser.add_argument("--wandb_key", type=str, default="", help="wandb key to login")
 parser.add_argument("--sample_during_training", type=bool, default=False, help="whether to generate samples after each training epoch")
@@ -18,22 +18,29 @@ parser.add_argument("--n_samples", type=int, default=10, help="number of samples
 
 args = parser.parse_args()
 
+logging.basicConfig(level=logging.NOTSET)
+handle = ""
+logger = logging.getLogger(handle)
+
 # Training
 torch.manual_seed(42)
 ddpm = UNet(dropout=0.1)
 if args.use_wandb:
     wandb.login(key=args.wandb_key)
     wandb.init(project='ddpm')
-logging.info('Start training, epochs: {}...'.format(args.n_epochs))
+logger.info('Start training, epochs: {}...'.format(args.n_epochs))
 losses = train_func(ddpm, n_epochs=args.n_epochs, use_wandb=args.use_wandb, sample_during_training=args.sample_during_training)
-logging.info('Finished training!')
-logging.info('Loss on training set: {}'.format(losses[-1]))
+logger.info('Finished training!')
+logger.info('Loss on training set: {}'.format(losses[-1]))
 
 # Generate samples
+logger.info('Generating {} samples...'.format(args.n_samples))
 samples, steps = sample_func(ddpm, n_samples=args.n_samples, use_wandb=args.use_wandb)
+logger.info('Finished generating!')
 
 # Save samples
-samples_PIL = [Image.fromarray(sample[i].squeeze().numpy()) for sample in samples]
+logger.info('Save generated samples to generated_samples.jpg...')
+samples_PIL = [Image.fromarray(sample.squeeze().numpy()) for sample in samples]
 rows = 2
 cols = int(math.ceil(args.n_samples / rows))
 # Python PIL/Image make 3x3 Grid from sequence Images: https://stackoverflow.com/a/65583584 
@@ -42,4 +49,5 @@ grid = Image.new(mode='gray', size=(cols * w, rows * h))
 grid_w, grid_h = grid.size
 for i, img in enumerate(samples_PIL):
     grid.paste(img, box=(i%cols*w, i//cols*h))
-grid.save("generated samples.jpg")
+grid.save("generated_samples.jpg")
+logger.info('Done!')
