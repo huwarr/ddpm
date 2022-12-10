20815899c1939eccc1e55f1b7c2b247a6ca6cdb0
huwarr <mikhalmatova@gmail.com>
2022-12-10 03:11:41 +0300

    update requirements.txt

--- metrics/is.py
+++
@@ -1,8 +0,0 @@
-from torchmetrics.image.inception import InceptionScore
-
-
-def compute_is(generated_samples):
-    inception = InceptionScore()
-    # generate some images
-    inception.update(generated_samples)
-    return inception.compute().item()
